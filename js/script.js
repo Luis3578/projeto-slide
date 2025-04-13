725fb984d7d0c3fb8366e524b759b13e65cf1abc
Luis Henrique <luispess75@gmail.com>
2025-04-13 17:15:54 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const btnNext = document.querySelector('.next')
+const btnBack = document.querySelector('.back')
+
+const list = document.querySelector('.list')
+const thumb = document.querySelector('.thumb')
+const container = document.querySelector('.container')
+
+
+const mudarConteudo = (type) => {
+    const listItem = document.querySelectorAll('.list-item')
+    const thumbItem = document.querySelectorAll('.thumb-item')
+
+    if (type === 'next') {
+        list.appendChild(listItem[0])
+        thumb.appendChild(thumbItem[0])
+        container.classList.add('next')
+    } else {
+        list.prepend(listItem[listItem.length - 1])
+        thumb.prepend(thumbItem[thumbItem.length - 1])
+        container.classList.add('back')
+    }
+
+
+
+    setTimeout(() => {
+        container.classList.remove('next')
+        container.classList.remove('back')
+    }, 900);
+}
+
+
+
+
+
+
+
+
+
+
+
+btnNext.addEventListener('click', () => mudarConteudo('next'))
+btnBack.addEventListener('click', () => mudarConteudo('back'))
